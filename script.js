729e5c2ff00c80f06d52714c44ea8f3485767025
Aafreen Abdulla <aafreen.abdulla@strathmore.edu>
2024-03-21 12:31:22 +0300

    commit

--- script.js
+++ script.js
@@ -1,33 +1,11 @@
-// JavaScript for handling menu toggle
-function myMenuFunction() {
-    var navMenu = document.getElementById("navMenu");
-    if (navMenu.className === "nav-menu") {
-        navMenu.className += " responsive";
-    } else {
-        navMenu.className = "nav-menu";
-    }
-}
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
 
-// JavaScript for handling form switching
-var loginBtn = document.getElementById("loginBtn");
-var registerBtn = document.getElementById("registerBtn");
-var loginForm = document.getElementById("login");
-var registerForm = document.getElementById("register");
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
 
-function login() {
-    loginForm.style.left = "4px";
-    registerForm.style.right = "-520px";
-    loginBtn.className += " white-btn";
-    registerBtn.className = "btn";
-    loginForm.style.opacity = 1;
-    registerForm.style.opacity = 0;
-}
-
-function register() {
-    loginForm.style.left = "-510px";
-    registerForm.style.right = "5px";
-    loginBtn.className = "btn";
-    registerBtn.className += " white-btn";
-    loginForm.style.opacity = 0;
-    registerForm.style.opacity = 1;
-}
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
